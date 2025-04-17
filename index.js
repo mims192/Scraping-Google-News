c56628e00c187c09b42ca5d3249d3615d309167a
Mimansa Sahay <mimansa192@gmail.com>
2025-04-17 22:52:36 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
-mongoose.connect(process.env.URI,{})
-.then(()=>{
-  console.log("Connected to Mongodb");
-})
-.catch((err)=>{
-  console.error("Error connecting to MongoDB",err.message);
-});
-
-app.use(express.json());
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, backend is working!');
-});
 
 const stocks = [
   'MANAKSIA STEELS',
@@ -33,13 +19,40 @@ const stocks = [
   'ITI',
   'TERA SOFTWARE'
 ];
-for (const stock of stocks) {
-  scrapeNews(stock).then(saveNews);
-}
+
+const runScraper = async () => {
+  for (const stock of stocks) {
+    console.log(`Scraping ${stock}...`);
+    try {
+      const articles = await scrapeNews(stock);
+      await saveNews(articles);
+    } catch (err) {
+      console.error(`Error scraping ${stock}:`, err.message);
+    }
+  }
+  console.log(" All stocks scraped and saved.");
+  process.exit(0);
+};
+
+
+mongoose.connect(process.env.URI,{})
+.then(()=>{
+  console.log("Connected to Mongodb");
+  runScraper();
+})
+.catch((err)=>{
+  console.error("Error connecting to MongoDB",err.message);
+  
+});
 
 
 
+app.use(express.json());
 
+
+app.get('/', (req, res) => {
+  res.send('Hello, backend is working!');
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
